ea28382bd15e7b2a360f1e925a0f575ad25efc31
madhuri102938 <141306445+madhuri102938@users.noreply.github.com>
2023-09-29 19:37:38 +0530

    Add files via upload

---
+++ five.py
@@ -0,0 +1,10 @@
+# print the greatest number of given two numbers
+num1=int(input("enter first number:"))
+num2=int(input("enter second number:"))
+if(num1>=num2):
+    if(num1==num2):
+        print("num1 and num2 are equal")
+    else:
+            print("num1 is greater than the num2")
+else:
+    print("num2 is greater than the num1")            
